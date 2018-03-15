bb72da7a1dca3e4982069720246901ed102b49a9
Evan Fagerberg <adioevan@gmail.com>
2018-03-14 23:35:22 -0400

    Make sure seed is only about inserts
    Add new relationship definitions to reflect seed

--- movie_graph/models.py
+++ movie_graph/models.py
@@ -1,19 +1,33 @@
 from neomodel import (
-    IntegerProperty, StringProperty,
-    StructuredNode, RelationshipFrom
+    ArrayProperty,
+    IntegerProperty,
+    StringProperty,
+    StructuredNode,
+    StructuredRel,
+    RelationshipFrom
 )
-
-
-class Movie(StructuredNode):
-    name = StringProperty(unique_index=True, required=True)
-    tagline = StringProperty(required=False)
-    released = IntegerProperty(required=True)
-
-    directors = RelationshipFrom('movie_graph.models.Person', 'DIRECTED')
-    actors = RelationshipFrom('movie_graph.models.Person', 'ACTED_IN')
-    producers = RelationshipFrom('movie_graph.models.Person', 'PRODUCED')
-    reviewers = RelationshipFrom('movie_graph.models.Person', 'REVIEWED')
 
 
 class Person(StructuredNode):
     name = StringProperty(required=True)
+
+
+class ActorRel(StructuredRel):
+    roles = ArrayProperty(StringProperty())
+
+
+class ReviewerRel(StructuredRel):
+    summary = StringProperty(required=True)
+
+
+class Movie(StructuredNode):
+    title = StringProperty(unique_index=True, required=True)
+    tagline = StringProperty(required=False)
+    released = IntegerProperty(required=True)
+
+    directors = RelationshipFrom(Person, 'DIRECTED')
+    actors = RelationshipFrom(Person, 'ACTED_IN', model=ActorRel)
+    producers = RelationshipFrom(Person, 'PRODUCED')
+    reviewers = RelationshipFrom(Person, 'REVIEWED', model=ReviewerRel)
+    followers = RelationshipFrom(Person, 'FOLLOWS')
+    writers = RelationshipFrom(Person, 'WROTE')
--- movie_graph/tests/test_models.py
+++ movie_graph/tests/test_models.py
@@ -3,6 +3,6 @@ from movie_graph.models import Person, Movie
 
 def test_movies_has_relation_to_person():
     p = Person(name="Evan").save()
-    m = Movie(name="Star Wars", released=1977).save()
+    m = Movie(title="Star Wars", released=1977).save()
     m.directors.connect(p)
     assert m.directors.single() == p
